ded9391fa3fd7012ec311ad647a97221a3fb69c6
nolasconapoleao <nolascoamadonapoleao@gmail.com>
2015-11-16 18:04:25 +0000

    definicao de estruturas

---
+++ Risk/src/risk/Pais.java
@@ -0,0 +1,13 @@
+
+package risk;
+
+public class Pais {
+    
+    int Id;
+    String name;
+    int exercito;
+    boolean fronteira=false;
+    int rating;
+    
+    
+}
---
+++ Risk/src/risk/Risk.java
@@ -0,0 +1,10 @@
+package risk;
+
+public class Risk {
+agent {
+}
+    public static void main(String[] args) {
+    
+    }
+    
+}
